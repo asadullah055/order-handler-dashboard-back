2c14515caa705742dbb8eb6edb04dc8252ad5e36
Asadullah Ahmed <asadullahahmed01@gmail.com>
2024-09-22 11:27:38 +0600

    model update

--- src/model/orderModel.js
+++ src/model/orderModel.js
@@ -36,39 +36,11 @@ const orderSchema = new Schema(
       type: [Object],
       default: [],
     },
-    // claim submit mail date
-   /*  csmd: {
-      type: String,
-      default: "",
-    }, */
-   /*  caseNumber: {
-      type: String,
-      default: "",
-    }, */
-    approvedOrReject: {
-      type: String,
-      default: "",
-    },
-    arMailDate: {
-      type: Date,
-      default: "",
-    },
-    paidAmount: {
-      type: String,
-      default: "",
-    },
-    statementNoOrInvoiceCycle: {
-      type: String,
-      default: "",
-    },
     comment: {
       type: String,
       default: "",
     },
-    complainDetails: {
-      type: String,
-      default: "",
-    },
+   
   },
   { timestamps: true, versionKey: false }
 );
